258ded6494811bec62b0bf7def7de1ab5d0b4327
holymoses <ayal.moses@gmail.com>
2017-03-21 20:20:36 +0200

    hf

--- app/src/main/java/com/rotemarbiv/sicret/MainActivity.java
+++ app/src/main/java/com/rotemarbiv/sicret/MainActivity.java
@@ -19,7 +19,6 @@ public class MainActivity extends AppCompatActivity {
     EditText inputText;
 
     @Override
-
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
